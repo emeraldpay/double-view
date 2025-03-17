f61b3bb4eb6ee1369bb8ff3fdaab10807fa6e80d
kan <kan@emerald.cash>
2025-03-17 13:02:50 +0400

    problem: content-type and status are set incorrectly in spring-web

--- doubleview-spring-web/src/main/java/io/emeraldpay/doubleview/spring/web/DoubleView.java
+++ doubleview-spring-web/src/main/java/io/emeraldpay/doubleview/spring/web/DoubleView.java
@@ -1,6 +1,7 @@
 package io.emeraldpay.doubleview.spring.web;
 
 import io.emeraldpay.doubleview.DoubleViewRenderer;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
@@ -20,13 +21,18 @@ public class DoubleView implements View {
 
     @Override
     public String getContentType() {
-        return MediaType.TEXT_HTML.getType();
+        return MediaType.TEXT_HTML_VALUE;
     }
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         String rendered = renderer.render(componentName, (Map<String, Object>) model);
-        response.setStatus(200);
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null && status instanceof Integer) {
+            response.setStatus((int) status);
+        } else {
+            response.setStatus(200);
+        }
         response.getOutputStream().write(rendered.getBytes());
     }
 
